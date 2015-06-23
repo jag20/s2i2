b7fd9cf5cc0e97428f8187f3828713b7eb161b47
John Gomez <gomez.john.a@gmail.com>
2015-06-23 09:53:59 -0400

    finished temperature conversion file

--- src/temperature.py
+++ src/temperature.py
@@ -5,12 +5,16 @@ This is a python module that converts temperatures
 """
 
 def f_to_k(temp_f):
-#  temp_k = ((temp_f-32)*5./9.)+273.15
-  'this is a test'
-  'this is another test'
-  return temp_k
+  return ((temp_f-32)*5./9.)+273.15
+
+def k_to_c(temp_k):
+  return temp_k - 273.15
+
+def f_to_c(temp_f):
+  temp_k = f_to_k(temp_f)
+  return k_to_c(temp_k)
 
 a = float(raw_input('Enter a number: '))
-print(f_to_k(a))
+print(f_to_c(a))
 
 
