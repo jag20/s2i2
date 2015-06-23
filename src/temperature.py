d38afb75dd6f07f2a9dbe5cec2e605c92167821e
John Gomez <gomez.john.a@gmail.com>
2015-06-23 09:49:23 -0400

    this is a test

--- src/temperature.py
+++ src/temperature.py
@@ -5,7 +5,8 @@ This is a python module that converts temperatures
 """
 
 def f_to_k(temp_f):
-  temp_k = ((temp_f-32)*5./9.)+273.15
+#  temp_k = ((temp_f-32)*5./9.)+273.15
+  'this is a test'
   return temp_k
 
 a = float(raw_input('Enter a number: '))
